f5b8cfc2ef7f074d6346f209e5541b8036a01579
niezheng1 <niezheng1@jd.com>
2020-01-17 16:21:44 +0800

    翻转链表

--- src/main/java/list/reverseBetween.java
+++ src/main/java/list/reverseBetween.java
@@ -8,6 +8,7 @@ import java.util.List;
  * [1,2,3,4,5]
  * 2
  * 4
+ * [1,4,3,2,5]
  *
  *  [3,5]
  * 1
@@ -19,28 +20,31 @@ public class reverseBetween {
         if(head == null || head.next == null){
             return head;
         }
-        if(m == n){
-            return head;
+        //用于指向返回节点的pre节点
+        ListNode l = new ListNode(-1);
+        //永远指向要翻转链表的pre节点
+        l.next = head;
+        ListNode pre = l;
+        for (int i =1;i<m;i++){
+            pre = pre.next;
         }
-        ListNode res = head;
-        for (int i =1;i<m-1;i++){
-            head = head.next;
+        //1 2 3 4 5
+        //1 3 2 4 5
+        //1 4 3 2 5
+        //每次找到head节点的netx节点 然后头插到pre节点后面
+        head = pre.next;
+        for(int i = m; i < n; i++){
+            ListNode next = head.next;
+            head.next = next.next;
+            next.next = pre.next;
+            pre.next = next;
         }
-        ListNode l = head;
-        head =head.next;
-        ListNode r = head;
-        ListNode pre = null;
-        ListNode next = null;
-        while (n-m>0){
-            n--;
-            next = head.next;
-            head.next = pre;
-            pre = head;
-            head = next;
-        }
-        l.next = pre;
-        r.next = next;
-        return res;
+        return l.next;
+    }
+
+
+    public static void main(String[] args) {
+
     }
 
 }
