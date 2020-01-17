ec0243df9381ac6b82e9fe9b6e9afabe4ca44660
niezheng1 <niezheng1@jd.com>
2020-01-17 16:28:53 +0800

    翻转链表的解法2

--- src/main/java/list/reverseList.java
+++ src/main/java/list/reverseList.java
@@ -23,4 +23,21 @@ public class reverseList {
     }
 
 
+    public ListNode reverseList1(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode pre =  new ListNode(-1);
+        pre.next = head;
+        while (head.next != null) {
+            ListNode next = head.next;
+            head.next = next.next;
+            next.next = pre.next;
+            pre.next = next;
+        }
+        return pre.next;
+
+    }
+
+
 }
