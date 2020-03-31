d7eead6677392b3e6ab935441ac64da66c8426d0
niezheng1 <niezheng1@jd.com>
2020-03-31 10:56:27 +0800

    全排列

--- src/main/java/backtracking/getPermutation.java
+++ src/main/java/backtracking/getPermutation.java
@@ -11,14 +11,14 @@ public class getPermutation {
     List<Integer> res = new ArrayList<>();
     public String getPermutation(int n, int k) {
         List<Integer> list = new ArrayList<>();
-
-        dfs(1,n,list,k);
+        int[] flag = new int[n];
+        dfs(1,n,list,k,flag);
         StringBuilder s = new StringBuilder();
         res.forEach(s::append);
-        return s.toString();
+        return  s.toString();
     }
 
-  void dfs(int index, int n,List<Integer> list,int k){
+  void dfs(int index, int n,List<Integer> list,int k,int[]  flag){
         if(list.size() == n){
             ans.add(new ArrayList<>(list));
             if(ans.size() == k){
@@ -26,16 +26,21 @@ public class getPermutation {
             }
             return ;
         }
-        for (int i = index;i<=n;i++){
-            list.add(i);
-            dfs(index+1,n,list,k);
+        for (int i = 0;i<n;i++){
+            if(flag[i] == 1){
+                continue;
+            }
+            list.add(i+1);
+            flag[i] = 1;
+            dfs(index+1,n,list,k,flag);
+            flag[i] = 0;
             list.remove(list.size()-1);
         }
     }
 
     public static void main(String[] args) {
         getPermutation permutation = new getPermutation();
-        permutation.getPermutation(3,3);
+        System.out.println(permutation.getPermutation(3,3));
     }
 
 }
